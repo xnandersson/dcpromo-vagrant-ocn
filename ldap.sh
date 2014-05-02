bca311d6b639b53e7b47d4a69ef1e03f57adaec2
Niklas Andersson <niklas.andersson@openforce.se>
2014-05-02 21:24:27 +0200

    Added slapd-conf

--- ldap.sh
+++ ldap.sh
@@ -1,1 +1,8 @@
 echo "Provisioning ldap"
+cat > /tmp/slapd.debconf << EOF
+slapd slapd/password1 string Secret007!
+slapd slapd/password2 string Secret007!
+EOF
+/usr/bin/debconf-set-selections /tmp/slapd.debconf
+apt-get update
+apt-get install slapd ldap-utils -y
