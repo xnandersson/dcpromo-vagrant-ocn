29f372f3cc61a0dcd906365104208c984fb7c3fc
Niklas Andersson <niklas.andersson@ericsson.com>
2014-07-08 13:36:16 +0200

    Added OpenLDAP-stuff

--- provisioning-ldap.sh
+++ provisioning-ldap.sh
@@ -1,9 +1,9 @@
 echo "Provisioning ldap"
-echo "ldap" > /etc/hostname
-/bin/hostname -b ldap
+echo "ldap.openforce.org" > /etc/hostname
+/bin/hostname -b ldap.openforce.org
 cat > /etc/hosts << EOF
 127.0.0.1	localhost
-127.0.1.1	ldap
+127.0.1.1	ldap.openforce.org ldap
 EOF
 cat > /tmp/slapd.debconf << EOF
 slapd slapd/password1 string Secret007!
@@ -11,4 +11,17 @@ slapd slapd/password2 string Secret007!
 EOF
 /usr/bin/debconf-set-selections /tmp/slapd.debconf
 apt-get update
-apt-get install slapd ldap-utils -y
+apt-get install sudo-ldap -y
+apt-get install slapd ldap-utils sudo-ldap -y
+cp /usr/share/doc/sudo-ldap/schema.OpenLDAP /etc/ldap/schema/sudo.schema
+cat > /etc/ldap/ldap.conf << EOF
+BASE	dc=openforce,dc=org
+BINDDN	cn=admin,dc=openforce,dc=org
+URI	ldap://localhost
+EOF
+#service slapd stop
+#slapadd -v -u -d 1 -F /etc/ldap/slapd.d -l /vagrant/of.ldif
+#service slapd start
+ldapadd -x -w Secret007! -f /vagrant/of.ldif -D 'cn=admin,dc=openforce,dc=org'
+wget http://www.opensource.apple.com/source/sudo/sudo-28/sudo/sudoers2ldif -O /root/sudoers2ldif
+chmod +x /root/sudoers2ldif
--- provisioning-realmd.sh
+++ provisioning-realmd.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-_HOSTNAME=client006
+#
+# Add PPA for PackageKit
+# Link: https://launchpad.net/~ximion/+archive/packagekit
+#
+#add-apt-repository ppa:ximion/packagekit
+#apt-get update
+
+_HOSTNAME=client007
 
 echo "$_HOSTNAME" > /etc/hostname
 /bin/hostname -b $_HOSTNAME
 cat > /etc/hosts << EOF
 127.0.0.1	localhost
 127.0.1.1	$_HOSTNAME
-192.168.33.2	dc dc.openforce.org
+#192.168.33.2	dc dc.openforce.org
 192.168.33.100	$_HOSTNAME
 EOF
 
@@ -46,6 +53,7 @@ EOF
 #sudo tee -a /etc/apt/sources.list.d/ddebs.list
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 428D7C01
 #apt-get install realmd-dbgsym adcli-dbgsym krb5-user smbclient ldap-utils nmap tshark tcpdump -y
+#echo "deb http://archive.ubuntu.com/ubuntu/ trusty-proposed restricted main multiverse universe" >> /etc/apt/sources.list
 apt-get update
 apt-get install realmd adcli krb5-user smbclient ldap-utils nmap tshark tcpdump -y
 
@@ -53,7 +61,7 @@ apt-get install realmd adcli krb5-user smbclient ldap-utils nmap tshark tcpdump 
 # As realmd 0.15.0 is broken, we must replace with our own
 # compiled binary.
 #
-cp /vagrant/build/realmd /usr/lib/realmd/
+#cp /vagrant/build/realmd /usr/lib/realmd/
 
 cat > /etc/ldap/ldap.conf << EOF
 BASE	DC=OPENFORCE,DC=ORG
@@ -64,7 +72,8 @@ echo "supersede domain-name 'openforce.org';" >> /etc/dhcp/dhclient.conf
 echo "supersede domain-name-servers 192.168.33.2;" >> /etc/dhcp/dhclient.conf
 
 cat > /etc/resolv.conf << EOF
-nameserver 192.168.33.2
+nameserver 192.168.33.5
+nameserver 192.168.1.1
 search openforce.org
 domain openforce.org
 EOF
@@ -76,5 +85,23 @@ cat > /etc/krb5.conf << EOF
   dns_lookup_kdc = true
 EOF
 #
-# We do the join with: realm -v --client-software=sssd --membership-software=adcli OPENFORCE.ORG
-#apt-get install sssd-tools sssd libnss-sss libpam-sss adcli samba-common-bin -y
+# PackageKit
+#   apt-get install packagekit && killall aptd
+#
+# Join:
+#realm -v join --client-software=sssd --membership-software=adcli OPENFORCE.ORG
+#
+# /usr/share/lightdm/lightdm.conf.d/50-ubuntu
+#   greeter-show-manual-login=true 
+#
+# /etc/sssd/sssd.conf
+#   create_homedir = True
+#   access_provider = permit
+#
+# /etc/pam.d/common-session (BEFORE row with pam_sss.so)
+#   session required        pam_mkhomedir.so umask=0022 skel=/etc/skel/
+#
+# /etc/ssh/sshd_config (Make sure home directory is created)
+#   AllowGroups ssh-allowed group1 group2
+#
+# https://wiki.ubuntu.com/LightDM
